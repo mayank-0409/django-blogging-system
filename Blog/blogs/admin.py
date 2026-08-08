b16cab8285e9a7f978d42f1f5569cf0a957b2173
Mack-0409 <mayanksingh0409@gmail.com>
2026-08-08 21:18:48 +0530

    Blog App & Blog Model

---
+++ Blog/blogs/admin.py
@@ -0,0 +1,15 @@
+from unicodedata import category
+from django.contrib import admin
+from .models import Blog, Category
+
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author','status', 'is_featured',)
+    search_fields = ('id', 'title', 'status', 'category__category_name',)
+    list_editable = ('is_featured',)
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # django-blogging-system
+# 55:00
