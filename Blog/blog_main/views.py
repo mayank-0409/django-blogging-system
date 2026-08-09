5609876a01e8792e6bea11fedecdf7e6ad3a2817
Mack-0409 <mayanksingh0409@gmail.com>
2026-08-09 19:45:54 +0530

    Modify Data

--- Blog/blog_main/views.py
+++ Blog/blog_main/views.py
@@ -4,10 +4,13 @@ from blogs.models import Category, Blog
 
 def home(request):
     categories = Category.objects.all()
-    featured_posts = Blog.objects.filter(is_featured=True).order_by('updated_at')
-    print(featured_posts)
+    featured_posts = Blog.objects.filter(is_featured=True, status='published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False, status='published')
+
+    
     context = {
         'categories': categories,
         'featured_posts': featured_posts,   
+        'posts': posts,
     }
     return render(request, 'home.html', context)
