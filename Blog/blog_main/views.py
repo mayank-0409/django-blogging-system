60af42638222ba424ec6adfb2b3651361d56f8d9
Mack-0409 <mayanksingh0409@gmail.com>
2026-08-12 18:50:33 +0530

    About Us Done

--- Blog/blog_main/views.py
+++ Blog/blog_main/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from blogs.models import Category, Blog
+from About.models import About
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published').order_by('updated_at')
 
+    # Fetching About Us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
     context = {
         'featured_posts': featured_posts,
         'posts': posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
