30653af1a866a6adf7a33607326bdd88efc2283d
Mack-0409 <mayanksingh0409@gmail.com>
2026-08-17 21:52:20 +0530

    create table in categories

--- Blog/dashboard/views.py
+++ Blog/dashboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from blogs.models import Blog, Category
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login')
 def dashboard(request):
     category_count = Category.objects.all().count()
     blogs_count = Blog.objects.all().count()
@@ -12,3 +14,7 @@ def dashboard(request):
         'blogs_count': blogs_count,
     } 
     return render(request, 'dashboard/dashboard.html', context)
+
+
+def categories(request):
+    return render(request, 'dashboard/categories.html')
