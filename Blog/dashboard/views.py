d8b6bb3d8a9f5b310128470b4383b81adf47eb14
Mack-0409 <mayanksingh0409@gmail.com>
2026-08-21 15:21:09 +0530

    Manager Dashboard

--- Blog/dashboard/views.py
+++ Blog/dashboard/views.py
@@ -3,6 +3,7 @@ from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm, BlogPostForm
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -98,3 +99,10 @@ def delete_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect('posts')
+
+def users(request):
+    users = User.objects.all()
+    context = {
+        'users': users,
+    }
+    return render(request, 'dashboard/users.html', context)
