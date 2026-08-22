135ed164fc9a6dac2a77b693820ce1639a300236
Mack-0409 <mayanksingh0409@gmail.com>
2026-08-23 00:17:33 +0530

    Add Comments

--- Blog/blogs/admin.py
+++ Blog/blogs/admin.py
@@ -1,6 +1,6 @@
 from unicodedata import category
 from django.contrib import admin
-from .models import Blog, Category
+from .models import Blog, Category, Comment
 
 # Register your models here.
 
@@ -12,4 +12,5 @@ class BlogAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment)
 
